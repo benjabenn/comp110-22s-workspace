900989384e59e80116c5faf40eff9359f78826d2
Benjamin Eldridge <beldridge@unc.edu>
2022-02-03 00:04:53 -0500

    Finished LS14

---
+++ lessons/function_def.py
@@ -0,0 +1,16 @@
+"""An example function definition."""
+
+
+# Line 4 is a Signature line "contract", line 5 is the docstring, lines 6-9 (nice) are the body block 
+def my_max(a: int, b: int) -> int:
+    """Returns the largest argument."""
+    if a >= b:
+        return a
+    else:
+        return b
+
+
+x: int = 7
+y: int = 6
+z: int = my_max(x, y)
+print(z)
